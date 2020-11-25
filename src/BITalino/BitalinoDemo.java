55be21264783c744117d70d6049704ecdc1d5856
ClaraU <ClaraU@10.60.90.93>
2020-11-25 12:52:32 +0100

    bitalino

--- src/BITalino/BitalinoDemo.java
+++ src/BITalino/BitalinoDemo.java
@@ -20,24 +20,24 @@ public class BitalinoDemo {
             bitalino = new BITalino();
             // Code to find Devices
             //Only works on some OS
-            Vector<RemoteDevice> devices = bitalino.findDevices();
-            System.out.println(devices);
+            //Vector<RemoteDevice> devices = bitalino.findDevices();
+            //System.out.println(devices);
 
             //You need TO CHANGE THE MAC ADDRESS
             //You should have the MAC ADDRESS in a sticker in the Bitalino
-            String macAddress = "20:16:02:14:75:76";
+            String macAddress = "98:D3:31:FD:3B:92";
             
             //Sampling rate, should be 10, 100 or 1000
-            int SamplingRate = 10;
+            int SamplingRate = 100;
             bitalino.open(macAddress, SamplingRate);
 
             // Start acquisition on analog channels A2 and A6
             // For example, If you want A1, A3 and A4 you should use {0,2,3}
-            int[] channelsToAcquire = {1, 5};
+            int[] channelsToAcquire = {0}; //We only want Electromyogram channel, that is A1
             bitalino.start(channelsToAcquire);
 
             //Read in total 10000000 times
-            for (int j = 0; j < 10000000; j++) {
+            for (int j = 0; j < 10000; j++) { //Record 10 seconds
 
                 //Each time read a block of 10 samples 
                 int block_size=10;
