fea535550edce732415ef028a31e4abfe4ad3961
ClaraU <ClaraU@10.60.90.93>
2020-12-15 17:26:48 +0100

    Cambios en RehabilitationProjectServer class inside the interface

--- RehabilitationProjectServer/src/rehabilitationprojectserver/RehabilitationProjectServer.java
+++ RehabilitationProjectServer/src/rehabilitationprojectserver/RehabilitationProjectServer.java
@@ -30,8 +30,9 @@ public class RehabilitationProjectServer extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         launch(args);
-    }
+        
+    }*/
     
 }
